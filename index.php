f7aaf8ee5fcb96cceed641a7310cac3a3a301ee7
Mongoose90 <shevkun90@mail.ru>
2014-08-18 22:16:17 +0300

    Добавил main.css, выравнял поля, сделал отмену флоат для картинки

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 <html>
 
 <head>
-
-<title>Homepage</title>
-
+	<meta charset="utf-8">
+	<title>Homepage</title>
+	<link rel="stylesheet" type="text/css" href="/css/main.css"> <!--Подключение Css-->
 </head>
 
 <body>
-
-<p>Hello world</p>
+	<h1> Vk.loc </h1>
+	<p>Hello world</p>
+	<a href="registration.php"> Зарегистрироваться </a> 
 
 </body>
 
---
+++ registration.php
@@ -0,0 +1,38 @@
+<DOCTYPE html>
+<html>
+	<head>
+			<meta charset="utf-8"> <!--Распознавание языка-->
+			<title> Регистрация </title> <!--Заголовок-->
+			 <link rel="stylesheet" type="text/css" href="/css/main.css"> <!--Подключение Css-->
+	</head>
+	<body >
+			<h1> Регистрация </h1>  <!--Заголовок-->
+			<form> 
+				<div class="main">
+					<div class="field">
+						<label for="n">Поле для  ввода email</label>
+						<input type="text" id="n" />
+					</div>
+					<div class="field">
+						<label for="n">Поле для ввода пароля</label>
+						<input type="password" id="n" />
+					</div>
+					<div class="field">
+						<label for="n">Поле для ввода логина</label>
+						<input type="text" id="ln" />
+					</div>
+					 <div class="field">
+						<label for="n">Регистрация</label>
+						<input type="submit" id="a" />
+					</div>
+					 <div class="field">
+						<label for="a">Запомнить</label>
+						<input type="checkbox" id="b" />
+					</div>
+				</div>
+			</form>  <!--Формы-->
+			<div class="field">	</div> <!--Отмена флоат-->
+			<img src="/image/vk.loc.jpg"> <!--Картинка-->
+			<a href="index.php"> Ссылка на сайт </a> <!--Ссылка-->
+	</body>
+</html>
