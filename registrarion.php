98ddbfeefe7234903ecd7a59c9e396d8abfe6b00
Mongoose90 <shevkun90@mail.ru>
2014-08-18 21:16:39 +0300

    Обновили код

--- registrarion.php
+++ registrarion.php
@@ -1,15 +1,20 @@
 <DOCTYPE html>
 <html>
 	<head>
-	<body bgcolor="#5F9EA0">
 			<meta charset="utf-8"> <!--Распознавание языка-->
-			<title> registration.php </title> <!--Заголовок-->
+			<title> Регистрация </title> <!--Заголовок-->
 			<style> img {border: 3px solid #000000} </style> <!--Рамка для картинки-->
 	</head>
-	<body>
+	<body bgcolor="#5F9EA0">
 			<h1> Регистрация </h1>  <!--Заголовок-->
-			<form> Поле для  ввода email <input type ="text"> Поле для ввода пароля <input type ="password"> Поле для ввода логина <input type ="text"> <input type ="submit" value ="Регистрация"> Запомнить <input type ="checkbox"> </form>  <!--Формы-->
-			<img src="http://autel-russia.ru/upload/medialibrary/5b8/5b8c304397e3ec16b929e34f8dbc8193.png"> <!--Картинка-->
+			<form>
+				Поле для  ввода email <input type ="text"> 
+				Поле для ввода пароля <input type ="password"> 
+				Поле для ввода логина <input type ="text">
+				<input type ="submit" value ="Регистрация"> 
+				Запомнить <input type ="checkbox"> 
+			</form>  <!--Формы-->
+			<img src="/image/registration.png"> <!--Картинка-->
 			<a href="vk.log"> Ссылка на сайт </a> <!--Ссылка-->
 	</body>
 </html>
