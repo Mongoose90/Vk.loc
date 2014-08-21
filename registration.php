395a0ed8ab7ff54a23e233498cb644a4b30e1fc8
Mongoose90 <shevkun90@mail.ru>
2014-08-21 21:31:36 +0300

    Добавил: php запрос для отправки данных в mysql, формы менял местами, добавлял указатели на имена для того чтобы они передались в php запрос, меняли get запрос на post

--- registration.php
+++ registration.php
@@ -1,3 +1,10 @@
+<?php 
+if(!empty($_POST)) {
+ $db = new PDO('mysql:host=localhost;dbname=vkloc;charset=utf8', 'vkloc', '12345');
+ $db->query('insert into users (email, login, password, date) values ("' . $_POST['email'] . '", "' . $_POST['login'] . '", "' . $_POST['password'] .'" , "' . $_POST['date'] . '")');
+ $thankYouMessage = $_POST['login'] . ', You are Welcome';
+}
+?>
 <DOCTYPE html>
 <html>
 	<head>
@@ -7,31 +14,35 @@
 	</head>
 	<body >
 			<h1> Регистрация </h1>  <!--Заголовок-->
-			<form> 
-				<div class="main">
+			<form method="post"> 
+				 <div class="main"> <!--Подключение сss, для выравнивания полей-->
 					<div class="field">
-						<label for="n">Поле для  ввода email</label>
-						<input type="text" id="n" />
+						<label for="email">Поле для  ввода email</label>
+						<input type="text" id="email" name="email"/>
 					</div>
 					<div class="field">
-						<label for="n">Поле для ввода пароля</label>
-						<input type="password" id="n" />
+						<label for="login"> Поле для ввода логина</label>
+						<input type="text" id="login" name="login" />
 					</div>
 					<div class="field">
-						<label for="n">Поле для ввода логина</label>
-						<input type="text" id="ln" />
+						<label for="password">Поле для ввода пароля</label>
+						<input type="password" id="password" name="password" />
+					</div>
+					<div class="field">
+						<label for="date"> Дата рождения </label>
+						<input type="text" id="date" name="date"/>
 					</div>
 					 <div class="field">
-						<label for="n">Регистрация</label>
-						<input type="submit" id="a" />
+						<label for="submit"> </label> <!--Кнопка-->
+						<input type="submit" id="submit" />
 					</div>
 					 <div class="field">
-						<label for="a">Запомнить</label>
-						<input type="checkbox" id="b" />
+						<label for="checkbox">Запомнить</label>
+						<input type="checkbox" id="checkbox" name="checkbox" />
 					</div>
 				</div>
 			</form>  <!--Формы-->
-			<div class="field">	</div> <!--Отмена флоат-->
+			<div class="field">	</div> <!--Отмена флоат для картинки-->
 			<img src="/image/vk.loc.jpg"> <!--Картинка-->
 			<a href="index.php"> Ссылка на сайт </a> <!--Ссылка-->
 	</body>
